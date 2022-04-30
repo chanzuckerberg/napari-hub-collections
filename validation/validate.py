f2bd785deb56478d0c2e901a8b7e78ecb6412fdf
Justin Kiggins <justin.kiggins@chanzuckerberg.com>
2022-04-29 17:43:00 -0700

    let it fail

--- validation/validate.py
+++ validation/validate.py
@@ -16,22 +16,22 @@ def validate(collection_path):
     collections = glob.glob(f"{collection_path}/*.yml")
 
     for collection in collections:
-        print(f"opening {collection}")
 
         # load the Collection
         with open(collection, "r") as stream:
             try:
-                print("loading...")
                 in_data = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
 
         # validate it
-        try:
-            CollectionSchema().load(in_data)
-        except ValidationError as err:
-            pprint(err.messages)
+        # try:
+        #     CollectionSchema().load(in_data)
+        # except ValidationError as err:
+        #     pprint(err.messages)
 
+        print(f"validating {collection}")
+        CollectionSchema().load(in_data)
 
 
 if __name__ == '__main__':
