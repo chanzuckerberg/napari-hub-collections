dc48b3cbce2a3d56a09814dbd8b35d728d973cd9
Kevin Lai <klai@chanzuckerberg.com>
2022-12-13 15:18:50 -0600

    code review changes

--- validation/validate.py
+++ validation/validate.py
@@ -15,41 +15,40 @@ from schema import CollectionSchema
 @click.argument('collection_path', nargs=1)
 def validate(collection_path):
     collections = glob.glob(f"{collection_path}/*.yml")
-    # list of .yml files that have errors
     yaml_error_list = []
     schema_error_list = []
 
     for collection in collections:
-
-        # load the Collection
         with open(collection, "r") as stream:
             try:
                 in_data = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                yaml_error_list.append((str(exc.problem) + str(exc.problem_mark)))
+                CollectionSchema.parse_obj(in_data)
+            except yaml.YAMLError as err:
+                yaml_error_list.append(f'{err.problem} {err.problem_mark}')
                 continue
-
-        # process the Collection
-        try:
-            CollectionSchema.parse_obj(in_data)
-        except ValidationError as err:
-            schema_error_list.append(str(err) + ' in ' + collection)
+            except ValidationError as err:
+                schema_error_list.append(f'{err} in {collection}')
 
     if len(yaml_error_list) == 0 and len(schema_error_list) == 0:
         print("All collections have been validated successfully")
         return
 
     if len(yaml_error_list) > 0:
-        print("Yaml Error List")
-        for item in yaml_error_list:
-            print(item)
+        print('Yaml Error List')
+        print(*yaml_error_list, sep='\n')
+    else:
+        print('No Yaml Errors')
+
     print("-" * 40)
+
     if len(schema_error_list) > 0:
         print("Schema Error List")
-        for item in schema_error_list:
-            print(item)
+        print(*schema_error_list, sep='\n')
+    else:
+        print("No Schema Errors")
+
     sys.exit(1)
 
 
 if __name__ == '__main__':
-    validate()
+    validate('/Users/klai/Documents/napari-hub-collections/collections')
