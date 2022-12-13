1724cd91e9269d47dea41de2a3452adce47e9c3b
Kevin Lai <70177777+klai95@users.noreply.github.com>
2022-12-13 16:02:13 -0600

    Collections - Validation Fix (#36)
    * fix validation for collections
    
    * deleted test case files
    
    * code review changes
    
    * code review changes
    
    * code review changes

--- validation/validate.py
+++ validation/validate.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python
 
+import sys
+
 import click
 import yaml
 import glob
 
+from pydantic import ValidationError
+
 from schema import CollectionSchema
+
 
 @click.command()
 @click.argument('collection_path', nargs=1)
 def validate(collection_path):
-
     collections = glob.glob(f"{collection_path}/*.yml")
+    yaml_error_list = []
+    schema_error_list = []
 
     for collection in collections:
-
-        # load the Collection
         with open(collection, "r") as stream:
             try:
                 in_data = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
+                CollectionSchema.parse_obj(in_data)
+            except yaml.YAMLError as err:
+                yaml_error_list.append(f'{err.problem} {err.problem_mark}')
+            except ValidationError as err:
+                schema_error_list.append(f'{err} in {collection}')
 
-        # validate it
-        # try:
-        #     CollectionSchema().load(in_data)
-        # except ValidationError as err:
-        #     pprint(err.messages)
+    if len(yaml_error_list) == 0 and len(schema_error_list) == 0:
+        print("All collections have been validated successfully")
+        return
 
-        print(f"validating {collection}")
-        parsed = CollectionSchema.parse_obj(in_data)
-        print(parsed)
+    if len(yaml_error_list) > 0:
+        print('Yaml Error List')
+        print(*yaml_error_list, sep='\n')
+    else:
+        print('No Yaml Errors')
+
+    print("-" * 40)
+
+    if len(schema_error_list) > 0:
+        print("Schema Error List")
+        print(*schema_error_list, sep='\n')
+    else:
+        print("No Schema Errors")
+
+    sys.exit(1)
 
 
 if __name__ == '__main__':
